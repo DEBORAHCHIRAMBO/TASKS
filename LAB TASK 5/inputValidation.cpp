5a398314a2b72d48441a160cbbfca97f53992ba7
DEBORAHCHIRAMBO <bsc-com-ne-19-21@unima.ac.mw>
2025-04-08 22:41:42 +0200

    Update inputValidation.cpp
    I had issues somewhere, so I decided to edit

--- LAB TASK 5/inputValidation.cpp
+++ LAB TASK 5/inputValidation.cpp
@@ -1,29 +1,18 @@
 #include <iostream>
-#include <limits>
-
 using namespace std;
 
-int main() {
-    int number = 0;
-
-    while (true) {
-        cout << "Enter a number between 5 and 10: ";
+int main(){
+    int number;
+    cout << "Enter an integer value between 5 and 10: ";
+    while (true){
         cin >> number;
-
-        // Check for invalid input
-        if (cin.fail()) {
-            cin.clear(); // Clear the error state
-            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Discard invalid input
-            cout << "You entered an invalid input. Please enter a valid number.\n";
-        } 
-        else if (number < 5 || number > 10) {
-            cout << "You entered an invalid number. Please try again.\n";
-        } 
-        else {
-            break; 
+        if (number >= 5 && number <= 10){
+        cout << "You entered " << number << " Has been accepted." <<endl;
+        break;
         }
+        else{
+            cout << "Sorry. You enterer an invalid number.Please try again" << endl;
+       }
     }
-
-    cout << "You input value: " << number << " has been accepted.\n";
     return 0;
 }
